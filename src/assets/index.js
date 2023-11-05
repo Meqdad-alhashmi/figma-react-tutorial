15741ef4c5c9e21f5d2cdc5bb180fdaf4ab76648
Meqdadalhashmi <meqdad000@gmail.com>
2023-11-05 12:01:47 +0300

    SetUp+Header

---
+++ src/assets/index.js
@@ -0,0 +1,47 @@
+import facebookLogo from "./facebook-logo.svg";
+import iconsVideoFile from "./flat-color-icons_video-file.svg";
+import frame4 from "./frame4.svg";
+import group7 from "./group7.svg";
+import group8 from "./group8.svg";
+import instagramLogo from "./instagram-logo.svg";
+import menuIcon from "./menu-icon.svg";
+import avatar from "./noto_student-light-skin-tone.svg";
+import rectangle8 from "./rectangle8.png";
+import rectangle13 from "./rectangle13.png";
+import rectangle14 from "./rectangle14.png";
+import rectangle15 from "./rectangle15.png";
+import rectangle16 from "./rectangle16.png";
+import rectangle17 from "./rectangle17.png";
+import rectangle18 from "./rectangle18.png";
+import rectangle19 from "./rectangle19.png";
+import rectangle20 from "./rectangle20.png";
+import rectangle21 from "./rectangle21.png";
+import thumbnail from "./thumbnail.png";
+import thumbnail1 from "./thumbnail1.png";
+import thumbnail2 from "./thumbnail2.png";
+import twitterLogo from "./twitter-logo.svg";
+
+export {
+  facebookLogo,
+  iconsVideoFile,
+  frame4,
+  group7,
+  group8,
+  instagramLogo,
+  menuIcon,
+  avatar,
+  rectangle8,
+  rectangle13,
+  rectangle14,
+  rectangle15,
+  rectangle16,
+  rectangle17,
+  rectangle18,
+  rectangle19,
+  rectangle20,
+  rectangle21,
+  thumbnail,
+  thumbnail1,
+  thumbnail2,
+  twitterLogo,
+};
